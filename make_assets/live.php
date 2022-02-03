98ada8010bb6967f3fbf4bc3bf0444f5e1390dc5
Zi Xing <zixing.narrakas1996@gmail.com>
2022-02-03 17:59:26 -0500

    Finalized boilerplate

--- make_assets/live.php
+++ make_assets/live.php
@@ -3,4 +3,4 @@
     require('ppm');
     import('net.intellivoid.dynamical_web');
 
-    \DynamicalWeb\DynamicalWeb::exec('net.intellivoid.intellivoid_website==latest');
+    \DynamicalWeb\DynamicalWeb::exec('net.intellivoid.services_web==latest');
